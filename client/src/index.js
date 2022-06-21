310e6b593f603d7442e8d7820d6ba56e09522e18
a-w-m <mamrey@gmail.com>
2022-06-21 09:48:06 -0400

    use fetch api

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
-import axios from "axios";
 import populate_articles from "./articles.js";
 import populate_sidebar from "./sidebar.js";
 import "./style/styles.css";
 
 (async () => {
-	const response = await axios.get("/api");
-	return response.data;
+	const response = await fetch("/api");
+	const json = await response.json();
+	return json;
 })().then((res) => {
 	populate_sidebar(res);
 	populate_articles(res);
